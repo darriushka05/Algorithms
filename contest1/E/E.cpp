1be794c8c514e9888d52327620a710ad9fe94b5a
golubtsova.dm@phystech.edu <golubtsova.dm@phystech.edu>
2023-06-13 16:50:08 +0300

    fix taskE

--- contest1/E/E.cpp
+++ contest1/E/E.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 
 const int kMaxN = 1e6 + 1;
 
-long long GetMaxArea(int n, long long* heights) {
+long long GetMaxArea(const std::vector<int64_t>& heights) {
   std::stack<std::pair<long long, long long>> increasing_sequence;
   increasing_sequence.push({-1, -1});
   long i = 0;
   long long max_area = 0;
-  while (i < n) {
+  while (i < heights.size()) {
     if (heights[i] > increasing_sequence.top().first) {
       increasing_sequence.push({heights[i], i});
       ++i;
@@ -24,13 +25,13 @@ long long GetMaxArea(int n, long long* heights) {
 
 int main() {
   std::ios_base::sync_with_stdio(false);
-  std::cin.tie(NULL);
+  std::cin.tie(nullptr);
   int n;
   std::cin >> n;
-  long long heights[kMaxN];
+  std::vector<int64_t> heights (kMaxN);
   for (int i = 0; i < n; ++i) {
     std::cin >> heights[i];
   }
   heights[n] = 0;
-  std::cout << GetMaxArea(n + 1, heights);
+  std::cout << GetMaxArea(heights);
 }
